39f0b636c2c53e3ab7bec01a4bc8579ccc32d9e0
Serdar Tumgoren <zstumgoren@gmail.com>
2024-04-10 08:34:10 -0700

    Rework to use Site class per agency. #3 #4

--- clean/ca/san_diego_pd.py
+++ clean/ca/san_diego_pd.py
@@ -1,72 +1,87 @@
 import time
 from pathlib import Path
 
-import requests
 from bs4 import BeautifulSoup
 
 from .. import utils
 from ..cache import Cache
 
 
-def scrape(data_dir=utils.CLEAN_DATA_DIR, cache_dir=utils.CLEAN_CACHE_DIR, throttle=0):
-    """Scrape San Diego Police Department for SB16/SB1421/AB748 data."""
-    cache = Cache(cache_dir)
-    # This module
-    mod = Path(__file__)
-    state_postal = mod.parent.stem
-    # Use module to construct agency slug, which we'll use downstream
-    # to create a subdir inside the main cache directory to stash files for this agency
-    cache_suffix = f"{state_postal}_{mod.stem}"  # ca_san_diego_pd
-    # Page with links to all the SB16/SB1421/AB748 "child" pages containing videos and documents
-    base_url = "https://www.sandiego.gov/police/data-transparency/mandated-disclosures/sb16-sb1421-ab748"
-    current_page = 0
-    page_count = None  # which we don't know until we get the first page
-    # This will be a list of paths to HTML pages that we cache locally
-    index_pages = download_index_pages(
-        base_url, cache, cache_suffix, throttle, page_count, current_page
-    )
-    # TODO: Get the child pages and, you know, actually scrape them
-    return index_pages
+class Site:
+    """Scrape file metadata and download files for the San Diego Police Department for SB16/SB1421/AB748 data.
 
-
-def download_index_pages(
-    base_url, cache, cache_suffix, throttle, page_count, current_page, index_pages=[]
-):
-    """Download index pages for SB16/SB1421/AB748.
-
-    Index pages link to child pages containing videos and
-    other files related to use-of-force and disciplinary incidents.
-
-    Returns:
-        List of path to cached index pages
+    Attributes:
+        name (str): The official name of the agency
     """
-    # Pause between requests
-    time.sleep(throttle)
-    file_stem = base_url.split("/")[-1]
-    base_file = f"{cache_suffix}/{file_stem}_page{current_page}.html"
-    # Download the page (if it's not already cached)
-    cache_path = cache.download(base_file, base_url, "utf-8")
-    # Add the path to the list of index pages
-    index_pages.append(cache_path)
-    # If there's no page_count, we're on first page, so...
-    if not page_count:
-        # Extract page count from the initial page
-        html = cache.read(base_file)
-        soup = BeautifulSoup(html, "html.parser")
-        page_count = int(
-            soup.find_all("li", class_="pager__item")[-1]  # last <li> in the pager
-            .a.attrs["href"]  # the <a> tag inside the <li>  # will be ?page=X
-            .split("=")[-1]  # get the X
-        )
-    if current_page != page_count:
-        # Recursively call this function to get the next page
-        next_page = current_page + 1
-        download_index_pages(
-            base_url, cache, cache_suffix, throttle, page_count, next_page
-        )
-    return index_pages
+
+    name = "San Diego Police Department"
+
+    def __init__(self, data_dir=utils.CLEAN_DATA_DIR, cache_dir=utils.CLEAN_CACHE_DIR):
+        """Initialize a new instance.
+
+        Args:
+            data_dir (Path): The directory where downstream processed files/data will be saved
+            cache_dir (Path): The directory where files will be cached
+        """
+        # Start page contains list of "detail"/child pages with links to the SB16/SB1421/AB748 videos and files
+        # along with additional index pages
+        self.base_url = "https://www.sandiego.gov/police/data-transparency/mandated-disclosures/sb16-sb1421-ab748"
+        self.data_dir = data_dir
+        self.cache_dir = cache_dir
+        self.cache = Cache(cache_dir)
+        # Use module path to construct agency slug, which we'll use downstream
+        mod = Path(__file__)
+        state_postal = mod.parent.stem
+        # to create a subdir inside the main cache directory to stash files for this agency
+        self.cache_suffix = f"{state_postal}_{mod.stem}"  # ca_san_diego_pd
+
+    def scrape_meta(self, throttle=0):
+        """Gather metadata on downloadable files (videos, etc.)."""
+        current_page = 0
+        page_count = None  # which we don't know until we get the first page
+        # This will be a list of paths to HTML pages that we cache locally
+        index_pages = self._download_index_pages(throttle, page_count, current_page)
+        # TODO: Get the child pages and, you know, actually scrape file metadata
+        return index_pages
+
+    # Helper functions
+    def _download_index_pages(self, throttle, page_count, current_page, index_pages=[]):
+        """Download index pages for SB16/SB1421/AB748.
+
+        Index pages link to child pages containing videos and
+        other files related to use-of-force and disciplinary incidents.
+
+        Returns:
+            List of path to cached index pages
+        """
+        # Pause between requests
+        time.sleep(throttle)
+        file_stem = self.base_url.split("/")[-1]
+        base_file = f"{self.cache_suffix}/{file_stem}_index_page{current_page}.html"
+        # Construct URL: pages, including start page, have a page GET parameter
+        target_url = f"{self.base_url}?page={current_page}"
+        # Download the page (if it's not already cached)
+        cache_path = self.cache.download(base_file, target_url, "utf-8")
+        # Add the path to the list of index pages
+        index_pages.append(cache_path)
+        # If there's no page_count, we're on first page, so...
+        if not page_count:
+            # Extract page count from the initial page
+            html = self.cache.read(base_file)
+            soup = BeautifulSoup(html, "html.parser")
+            page_count = int(
+                soup.find_all("li", class_="pager__item")[-1]  # last <li> in the pager
+                .a.attrs["href"]  # the <a> tag inside the <li>  # will be ?page=X
+                .split("=")[-1]  # get the X
+            )
+        if current_page != page_count:
+            # Recursively call this function to get the next page
+            next_page = current_page + 1
+            self._download_index_pages(throttle, page_count, next_page, index_pages)
+        return index_pages
 
 
+"""
 # LEGACY CODE BELOW #
 def _scrape_list_page(cache, top_level_urls, base_url, throttle):
     second_level_urls = {}
@@ -105,7 +120,4 @@ def _download_case_files(base_url, second_level_urls):
             all_case_content_links.append(text)
             print("_______________________")
     return
-
-
-if __name__ == "__main__":
-    scrape()
+"""
