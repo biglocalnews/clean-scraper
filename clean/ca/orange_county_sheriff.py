c47c8a6c12f323449f10bd4905805f4a9732b557
jrynning <149276461+jrynning@users.noreply.github.com>
2024-04-21 19:26:39 -0700

    Update orange_county_sheriff.py

--- clean/ca/orange_county_sheriff.py
+++ clean/ca/orange_county_sheriff.py
@@ -1,5 +1,7 @@
 import time
+import urllib.parse
 from pathlib import Path
+from typing import List
 
 from bs4 import BeautifulSoup
 
@@ -31,20 +33,70 @@ class Site:
         self.data_dir = data_dir
         self.cache_dir = cache_dir
         self.cache = Cache(cache_dir)
+
+    @property
+    def agency_slug(self) -> str:
+        """Construct the agency slug."""
         # Use module path to construct agency slug, which we'll use downstream
         mod = Path(__file__)
         state_postal = mod.parent.stem
         # to create a subdir inside the main cache directory to stash files for this agency
-        self.cache_suffix = f"{state_postal}_{mod.stem}"  # ca_orange_county_sheriff
+        return f"{state_postal}_{mod.stem}"  # ca_orange_county_sheriff
 
     def scrape_meta(self, throttle=0):
         """Gather metadata on downloadable files (videos, etc.)."""
         current_page = 0
         index_pages = self._download_index_pages(throttle, current_page)
         # TODO: Get the child pages and, you know, actually scrape file metadata
-        return index_pages
+        downloadable_files = self._create_json(current_page)
+        return downloadable_files
+
+    # def scrape(self, metadata_csv):
 
     # Helper functions
+
+    def _create_json(self, current_page) -> Path:
+        metadata = []
+        file_stem = self.base_url.split("/")[-1]
+        # html_location = f"{self.agency_slug}/{file_stem}_index_page{current_page}.html"
+        html_location = self.cache.read(
+            f"{self.agency_slug}/{file_stem}_index_page{current_page}.html"
+        )
+        with open(html_location) as hl:
+            soup = BeautifulSoup(hl, "html.parser")
+        title = soup.find("title").text.strip()
+        links = soup.article.find_all("a")
+        urls = []
+        name = []
+        for link in links:
+            for link in links:
+                if "http" in link["href"]:
+                    urls.append(link["href"])
+        for url in urls:
+            url_to_name = url.split("Mediazip/")[-1]
+            url_to_name1 = url_to_name.replace("/", "_")
+            url_to_name2 = url_to_name1.replace(".zip", "")
+            url_to_name3 = url_to_name2.replace(
+                f"{url_to_name2}", f"Orange_County_Sheriffs_Department_{url_to_name2}"
+            )
+            url_to_name4 = url_to_name3.replace("%20", "_")
+            url_to_name5 = url_to_name4.strip()
+            name.append(url_to_name5)
+        url_dict = {name[i]: urls[i] for i in range(len(urls))}
+        for key, value in url_dict.items():
+            payload = {
+                "title": title,
+                "parent_page": str(html_location),
+                "asset_url": value,
+                "name": key,
+            }
+            metadata.append(payload)
+        # Store the metadata in a JSON file in the data directory
+        outfile = self.data_dir.joinpath(f"{self.agency_slug}.json")
+        self.cache.write_json(outfile, metadata)
+        # Return path to metadata file for downstream use
+        return outfile
+
     def _download_index_pages(self, throttle, current_page, index_pages=[]):
         """Download index pages for SB16/SB1421/AB748.
 
@@ -57,7 +109,7 @@ class Site:
         # Pause between requests
         time.sleep(throttle)
         file_stem = self.base_url.split("/")[-1]
-        base_file = f"{self.cache_suffix}/{file_stem}_index_page{current_page}.html"
+        base_file = f"{self.agency_slug}/{file_stem}_index_page{current_page}.html"
         # Construct URL: pages, including start page, have a page GET parameter
         target_url = f"{self.base_url}?page={current_page}"
         # Download the page (if it's not already cached)
