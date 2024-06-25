496db26b2f30a422683ffa28b505c7defcd79586
Gerald Rich <newsroomdev@users.noreply.github.com>
2024-06-25 16:06:38 -0700

    feat: collect zip & pdfs; todo: handle dupe assets

--- clean/ca/sacramento_pd.py
+++ clean/ca/sacramento_pd.py
@@ -1,8 +1,11 @@
+import copy
 import time
 from pathlib import Path
 from typing import List
+from urllib.parse import urlparse
+from pprint import pprint
 
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, ResultSet, Tag
 
 from .. import utils
 from ..cache import Cache
@@ -58,7 +61,6 @@ class Site:
         return downloadable_files
 
     def scrape(self, throttle: int = 0, filter: str = "") -> List[Path]:
-        # TODO: Refactor out scrape method in favor of Prefect flows
         metadata = self.cache.read_json(
             self.data_dir.joinpath(f"{self.agency_slug}.json")
         )
@@ -90,13 +92,19 @@ class Site:
         return self.cache.download(base_file, url, "utf-8")
 
     def _create_metadata_json(self) -> Path:
+        """
+        Creates a metadata JSON file containing information about the child links extracted from the HTML page.
+
+        Returns:
+            Path: The file path of the created metadata JSON file.
+        """
         links: List[MetadataDict] = []
         file_stem = self.disclosure_url.split("/")[-1]
         html_location = f"{self.agency_slug}/{file_stem}.html"
         html = self.cache.read(html_location)
         soup = BeautifulSoup(html, "html.parser")
         lists = soup.select("#container-392a98e5b6 .paragraph li")
-
+        print(f"retrieved {len(lists)} lists")
         for url in self._extract_index_urls(lists):
             links.append(
                 {
@@ -106,36 +114,62 @@ class Site:
                     "name": url["name"],
                 }
             )
+        print(f"extracted {len(links)} links from parent page")
 
         metadata = self._extract_child_links(links)
+        print(f"collected {len(metadata)} links total")
         outfile = self.data_dir.joinpath(f"{self.agency_slug}.json")
+        pprint(self._contains_repeated_asset_url(metadata))
         self.cache.write_json(outfile, metadata)
         return outfile
 
     def _extract_child_links(self, links: List[MetadataDict]) -> List[MetadataDict]:
-        # TODO: Refactor to account for zip files in CSI label
-        """Given a list of links, check for CSI images, extract links, and add to metadata."""
-        for link in links:
-            if "csi" in link["name"].lower():
-                url = link["asset_url"]
-                file_stem = url.split("/")[-2]
-                soup = self._download_and_soup(url, file_stem)
-                photo_links = self._extract_photos(soup, file_stem, link)
-                links.extend(photo_links)
-        return links
+        """
+        Given a list of links, check for child pages, extract links, and add to metadata.
 
-    def _download_and_soup(self, url: str, file_stem: str) -> BeautifulSoup:
-        """Download and parse a URL, returning a BeautifulSoup object."""
-        # TODO potentially include child subdirs
-        base_file = f"{self.agency_slug}/{file_stem}.html"
-        cache_path = self.cache.download(base_file, url, "utf-8")
-        html = self.cache.read(cache_path)
-        return BeautifulSoup(html, "html.parser")
+        Args:
+            links (List[MetadataDict]): A list of links containing metadata information.
+
+        Returns:
+            List[MetadataDict]: A modified list of links with additional CSI image links added to the metadata.
+        """
+        modified_links = copy.deepcopy(links)
+        for link in links:
+            if link["asset_url"].endswith("/"):
+                parsed_url = urlparse(link["asset_url"])
+                # Split URL and remove empty strings for trailing slash
+                url_split = [u for u in parsed_url.path.split("/") if u != ""]
+                if not self._is_asset(link["asset_url"]):
+                    file_stem = url_split[-1]
+                    soup = self._download_and_parse(link["asset_url"], file_stem)
+                    photo_links = self._extract_photos(soup, file_stem, link)
+                    print(f"\t extending links by {len(photo_links)}")
+                    modified_links.extend(photo_links)
+                else:
+                    modified_links.append(
+                        {
+                            "title": link["title"],
+                            "parent_page": link["parent_page"],
+                            "asset_url": link["asset_url"],
+                            "name": link["name"],
+                        }
+                    )
+        return modified_links
 
     def _extract_photos(
         self, soup: BeautifulSoup, file_stem: str, link: MetadataDict
     ) -> List[MetadataDict]:
-        """Extract photo links from a BeautifulSoup object and return a list of MetadataDict."""
+        """
+        Extract photo links from a BeautifulSoup object and return a list of MetadataDict.
+
+        Args:
+            soup (BeautifulSoup): The BeautifulSoup object representing the HTML page.
+            file_stem (str): The file stem of the current page.
+            link (MetadataDict): The metadata dictionary of the current page.
+
+        Returns:
+            List[MetadataDict]: A list of MetadataDict containing the extracted photo information.
+        """
         title_tag = soup.find("h1")
         photo_links = soup.select(".col-filename a")
         photos: List[MetadataDict] = []
@@ -143,7 +177,7 @@ class Site:
             if str(photo["href"]).endswith("/"):
                 child_url = f'{ASSET_URL}{photo["href"]}'
                 child_file_stem = child_url.split("/")[-2]
-                child_soup = self._download_and_soup(child_url, child_file_stem)
+                child_soup = self._download_and_parse(child_url, child_file_stem)
                 more_photos = self._extract_photos(child_soup, child_file_stem, link)
                 photos.extend(more_photos)
 
@@ -163,21 +197,97 @@ class Site:
 
         return photos
 
+    def _download_and_parse(self, url: str, file_stem: str) -> BeautifulSoup:
+        """
+        Download and parse a URL, returning a BeautifulSoup object.
+
+        Args:
+            url (str): The URL to download and parse.
+            file_stem (str): The stem of the file name to save the downloaded HTML.
+
+        Returns:
+            BeautifulSoup: The parsed HTML as a BeautifulSoup object.
+        """
+        base_file = f"{self.agency_slug}/{file_stem}.html"
+        cache_path = self.cache.download(base_file, url, "utf-8")
+        html = self.cache.read(cache_path)
+        return BeautifulSoup(html, "html.parser")
+
     def _clean_text(self, text: str) -> str:
+        """
+        Cleans the given text by replacing non-breaking spaces with regular spaces and removing leading/trailing whitespace.
+
+        Args:
+            text (str): The text to be cleaned.
+
+        Returns:
+            str: The cleaned text.
+        """
         return text.replace("\u00a0", " ").strip()
 
-    def _extract_index_urls(self, lists):
+    def _is_asset(self, url: str) -> bool:
+        """
+        Check if the given URL points to a PDF or ZIP file.
+
+        Args:
+            url (str): The URL to check.
+
+        Returns:
+            bool: True if the URL points to a PDF or ZIP file, False otherwise.
+        """
+        parsed_url = urlparse(url)
+        path = parsed_url.path
+        return path.endswith(".pdf") or path.endswith(".zip")
+
+    def _extract_index_urls(self, lists: ResultSet[Tag]):
+        """
+        Extracts the index URLs from a list of tags, accounting for relative URLs.
+
+        Args:
+            lists (ResultSet[Tag]): A list of tags containing the index URLs.
+
+        Yields:
+            dict: A dictionary containing the extracted information for each index URL.
+                The dictionary has the following keys:
+                - "title": The title of the index URL.
+                - "name": The name of the index URL.
+                - "href": The URL of the index link.
+
+        """
         for li in lists:
-            title = li.select_one("strong")
-            if title is None:
-                title = ""
-            else:
-                title = self._clean_text(f"{title.get_text()} {title.next_sibling}")
+            title_tag = li.select_one("strong")
+            title_str = ""
+            if title_tag is not None:
+                title_str = self._clean_text(
+                    f"{title_tag.get_text()} {title_tag.next_sibling}"
+                )
             links = li.find_all("a")
             for link in links:
-                if "http" in link["href"]:
-                    yield {
-                        "title": title,
-                        "name": self._clean_text(link.text),
-                        "href": link["href"],
-                    }
+                href_str = link["href"]
+                if "http" not in href_str:
+                    href_str = f"{self.base_url}{href_str}"
+                yield {
+                    "title": title_str,
+                    "name": self._clean_text(link.text),
+                    "href": href_str,
+                }
+
+    def _contains_repeated_asset_url(self, objects: List[MetadataDict]):
+        """
+        Checks if the given list of objects contains any repeated asset URLs.
+
+        Args:
+            objects (List[MetadataDict]): A list of objects, where each object is a dictionary containing metadata.
+
+        Returns:
+            set: A set of asset URLs that are repeated in the given list of objects.
+        """
+        seen_urls = set()
+        repeated_urls = set()
+        for obj in objects:
+            asset_url = obj.get("asset_url")
+            if asset_url in seen_urls:
+                repeated_urls.add(asset_url)
+            else:
+                seen_urls.add(asset_url)
+        return repeated_urls
