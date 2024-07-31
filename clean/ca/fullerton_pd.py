f2f7b5221d9d9169cdd5cc69456c3473a1c882dd
Nauman <157045300+naumansharifwork@users.noreply.github.com>
2024-07-31 14:19:15 +0500

    fixed the name of agency and fixed making of empty directory

--- clean/ca/fullerton_pd.py
+++ clean/ca/fullerton_pd.py
@@ -8,13 +8,13 @@ from ..cache import Cache
 
 
 class Site:
-    """Scrape file metadata and download files for the fullerton.
+    """Scrape file metadata and download files for the fullerton_pd.
 
     Attributes:
         name (str): The official name of the agency
     """
 
-    name = "Fullerton"
+    name = "Fullerton Police Department"
 
     def __init__(
         self,
@@ -75,7 +75,7 @@ class Site:
         base_output_json = self.cache_dir.joinpath(filename)
         base_output_json.parent.mkdir(parents=True, exist_ok=True)
         with utils.post_url(self.folder_url, json=self.folder_request_body) as r:
-            self.cache.write_json(filename, r.json())
+            self.cache.write_json(base_output_json, r.json())
 
         metadata = []
         base_json = self.cache.read_json(base_output_json)
@@ -86,16 +86,17 @@ class Site:
             filename = f"{self.agency_slug}/{result.get('name')}.json"
             output_json = self.cache_dir.joinpath(filename)
             with utils.post_url(self.folder_url, json=self.folder_request_body) as r:
-                self.cache.write_json(filename, r.json())
-                local_index_json.append(output_json)
+                self.cache.write_json(output_json, r.json())
+                output_dict = {"fileName": filename, "filePath": output_json}
+                local_index_json.append(output_dict)
         for download_json_path in local_index_json:
-            download_dict = self.cache.read_json(download_json_path)
+            download_dict = self.cache.read_json(download_json_path["filePath"])
             results = download_dict.get("data", {}).get("results", [])
             title = download_dict.get("data", {}).get("name", "")
             for result in results:
                 payload = {
                     "title": title,
-                    "parent_page": str(download_json_path),
+                    "parent_page": str(download_json_path["fileName"]),
                     "asset_id": result.get("entryId"),
                     "name": result.get("name"),
                 }
