84692e5923975744aa1adeb774c4cb07311f06b0
Mike Stucka <stucka@whitedoggies.com>
2024-09-06 05:47:30 -0400

    Working implementations

--- bart.py
+++ bart.py
@@ -9,11 +9,11 @@ from clean.platforms.nextrequest import process_nextrequest
 logger = logging.getLogger(__name__)
 logging.basicConfig(encoding="utf-8", level=logging.DEBUG)
 
-start_url = "https://lacity.nextrequest.com/documents?folder_filter=F031-22"
+start_url = "https://bart.nextrequest.com/requests/21-107"
 
 data_dir = utils.CLEAN_DATA_DIR
 cache_dir = utils.CLEAN_CACHE_DIR
-site_slug = "ca_los_angeles_pd"
+site_slug = "ca_bart_pd"
 subpages_dir = cache_dir / (site_slug + "/" + "subpages")
 
 cache = Cache(path=None)
@@ -21,4 +21,5 @@ cache = Cache(path=None)
 metadata = process_nextrequest(subpages_dir, start_url, force=False)
 json_filename = data_dir / (site_slug + ".json")
 cache.write_json(json_filename, metadata)
+
 # print(metadata)
