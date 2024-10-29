23d47ad85b940c0ef0240025cee5ceb39cfd11de
Nauman <157045300+naumansharifwork@users.noreply.github.com>
2024-10-29 17:00:18 +0500

    added comments

--- clean/ca/fullerton_pd.py
+++ clean/ca/fullerton_pd.py
@@ -30,7 +30,6 @@ class Site:
             "https://portal.laserfiche.com/Portal/Browse.aspx?id=726681&repo=r-3261686e"
         )
         self.folder_url = "https://portal.laserfiche.com/Portal/FolderListingService.aspx/GetFolderListing2"
-        self.folder_content_url = "https://portal.laserfiche.com/Portal/FolderListingService.aspx/GetFolderListing2"
         self.folder_request_body = {
             "repoName": "r-3261686e",
             "folderId": 726681,
@@ -40,19 +39,6 @@ class Site:
             "sortColumn": "",
             "sortAscending": True,
         }
-        self.start_export_url = (
-            "https://portal.laserfiche.com/Portal/ZipEntriesHandler.aspx/StartExport"
-        )
-        self.start_export_payload = {
-            "repoName": "r-3261686e",
-            "ids": [],
-            "key": -1,
-            "watermarkIdx": -1,
-        }
-        self.check_export_status_url = "https://portal.laserfiche.com/Portal/ZipEntriesHandler.aspx/CheckExportStatus"
-        self.download_exported_url = (
-            "https://portal.laserfiche.com/Portal/ExportJobHandler.aspx/GetExportJob/"
-        )
         self.data_dir = data_dir
         self.cache_dir = cache_dir
         self.cache = Cache(cache_dir)
@@ -73,14 +59,22 @@ class Site:
         filename = f"{self.agency_slug}/{base_name}"
         base_output_json = self.cache_dir.joinpath(filename)
         base_output_json.parent.mkdir(parents=True, exist_ok=True)
-        with utils.post_url(self.folder_url, json=self.folder_request_body) as r:
+        with utils.post_url(
+            self.folder_url, json=self.folder_request_body
+        ) as r:  # getting the Index Page Json by hitting the POST Api
             self.cache.write_json(base_output_json, r.json())
 
         metadata = []
-        base_json = self.cache.read_json(base_output_json)
+        base_json = self.cache.read_json(
+            base_output_json
+        )  # Reading The Index Page Json
         results = base_json.get("data", {}).get("results", [])
         local_index_json = []
-        for result in results:
+        for (
+            result
+        ) in (
+            results
+        ):  # For every result in the Index page which is a folder get the folder ID and add it to the folder_request_body and send the API call to get the next data
             self.folder_request_body["folderId"] = result.get("entryId")
             filename = f"{self.agency_slug}/{result.get('name')}.json"
             output_json = self.cache_dir.joinpath(filename)
@@ -89,13 +83,19 @@ class Site:
                 output_dict = {"fileName": filename, "filePath": output_json}
                 local_index_json.append(output_dict)
             time.sleep(throttle)
-        for download_json_path in local_index_json:
+        for (
+            download_json_path
+        ) in (
+            local_index_json
+        ):  # Once the Index Page Folders Jsons are downloaded Iterating over them
             download_dict = self.cache.read_json(download_json_path["filePath"])
             results = download_dict.get("data", {}).get("results", [])
             title = download_dict.get("data", {}).get("name", "")
             case_id = self._get_case_id(title)
-            for result in results:
-                if result.get("type") == -2 and result.get("mediaHandlerUrl") is None:
+            for result in results:  # This is for every folder within Index Page
+                if (
+                    result.get("type") == -2 and result.get("mediaHandlerUrl") is None
+                ):  # This check is for the PDF files
                     payload = {
                         "title": title,
                         "parent_page": str(download_json_path["fileName"]),
@@ -107,7 +107,8 @@ class Site:
                     metadata.append(payload)
                 elif (
                     result.get("type") == -2
-                    and result.get("mediaHandlerUrl") is not None
+                    and result.get("mediaHandlerUrl")
+                    is not None  # This check is for the media files like image, video, audio
                 ):
                     payload = {
                         "title": title,
@@ -117,7 +118,9 @@ class Site:
                         "details": {"extension": result.get("extension", None)},
                     }
                     metadata.append(payload)
-                elif result.get("type") == 0:
+                elif (
+                    result.get("type") == 0
+                ):  # This check is for the folder within a folder if there are folders with in folders it will download and get the child folders as well.
                     childMetadata_list = self._get_child_pages(
                         result, download_json_path["fileName"], title
                     )
@@ -165,7 +168,9 @@ class Site:
                     childMetadata.append(payload)
                 else:
                     childMetadata_list = self._get_child_pages(
-                        result, filename, parent_title
+                        result,
+                        filename,
+                        parent_title,  # Recursively go within folders to get to all the child folder data
                     )
 
                     for payload in childMetadata_list:
