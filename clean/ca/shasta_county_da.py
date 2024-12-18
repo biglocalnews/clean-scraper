add7b5f4991aad116d75bf1f97116bd1049ba86c
Irene Casado Sánchez <97088786+irenecasado@users.noreply.github.com>
2024-12-18 09:50:43 -0800

    Update shasta_county_da.py
    bs4.Tag out

--- clean/ca/shasta_county_da.py
+++ clean/ca/shasta_county_da.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from typing import List
 from urllib.parse import urljoin
 
-from bs4 import BeautifulSoup, Tag
+from bs4 import BeautifulSoup
 
 from .. import utils
 from ..cache import Cache
