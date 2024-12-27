3467fdda7f70637d3c6eeac63a2f7cc472135b3b
Mike Stucka <stucka@whitedoggies.com>
2024-12-27 10:51:40 -0500

    Patch fremontpd #169 move config

--- clean/ca/fremont_pd.py
+++ clean/ca/fremont_pd.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 
 from .. import utils
 from ..cache import Cache
-from ..config.fremont_pd import index_request_headers
+from .config.fremont_pd import index_request_headers
 
 
 class Site:
